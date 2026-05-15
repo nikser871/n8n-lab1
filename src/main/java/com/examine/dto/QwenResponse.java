bae9622cee42f2228c87315b2ed446f2c270b20a
nikser871 <nikser871@gmail.com>
2026-05-15 21:15:44 +0300

    ignore unknown properties

--- src/main/java/com/examine/dto/QwenResponse.java
+++ src/main/java/com/examine/dto/QwenResponse.java
@@ -1,8 +1,10 @@
 package com.examine.dto;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import io.micronaut.serde.annotation.Serdeable;
 
+@JsonIgnoreProperties
 @Serdeable
 public record QwenResponse(@JsonAlias("id") String articleId,
                            String summary) {}
